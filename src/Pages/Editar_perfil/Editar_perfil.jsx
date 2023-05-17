edb9bece5d6813f05f7543c9dd541f916ebf2cb2
Pedro Alonso <112452496+drinhope@users.noreply.github.com>
2023-05-17 18:12:16 -0300

    Back-end atualizado Aula 7 (rotas + arquivos de páginas)

---
+++ src/Pages/Editar_perfil/Editar_perfil.jsx
@@ -0,0 +1,13 @@
+function Editar_perfil() {
+
+  return (
+  
+    <div>
+      ESTÁ É A PÁGINA EDITAR_PERFIL
+    </div>
+  
+  )
+  
+}
+  
+export default Editar_perfil;
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+
+  return (
+  
+    <div>
+      ESTÁ É A PÁGINA LOGIN
+    </div>
+  
+  )
+  
+}
+  
+export default Login;
