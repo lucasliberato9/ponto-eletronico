98343a38fc2f3884fa5140c1ae3aa6c863f19f83
lucasliberato9 <lucasliberato2909@gmail.com>
2023-05-18 18:31:35 -0300

    primeiras alterações na página login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,21 @@
+import { Titulo, Form } from "./Styles";
+
 function Login() {
 
   return (
   
     <div>
-      ESTÁ É A PÁGINA LOGIN
+      <Titulo>
+        Faça aqui o seu login!
+      </Titulo>
+      <Form>
+        Email
+        <input></input>
+        Senha
+        <input></input>
+        <button>Fazer login</button>
+        <button>Quero me cadastrar</button>
+      </Form>
     </div>
   
   )
---
+++ src/Pages/Login/Styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Titulo = styled.div`
+    text-align: center;
+`
+export const Form = styled.div`
+    
+`
