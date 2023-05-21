52afaa49b897bf0598a58be11437a1f48c3ae876
Lucas Fernando <lchinuzinhoplay@gmail.com>
2023-05-21 20:32:20 -0300

    áreas de logar ponto

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,52 @@
-import { Container, Titulo } from "./Styles";
+import { Botaoadm,Pontoadm, Titulo1,Botaologado, PontoLogado, Titulo2, Labelj, Labelg, Labelb, Labela } from "./Styles";
 import { BotaoGenerico } from "../../components";
 
 function Home() {
 
   return (
-  
-    <Container>
-      <Titulo>
-        RODEI
-      </Titulo>
-    </Container>
-  
+  /*
+    <Pontoadm>
+      <Titulo1>
+        ADMINISTRE SEU PONTO
+      </Titulo1>
+      <Botaoadm> 
+        <BotaoGenerico name= "Login/Cadastre-se" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
+        </Botaoadm>
+    </Pontoadm>
+*/
+/*
+<PontoLogado>
+      <Titulo2>
+       LIGUE SEU PONTO
+      </Titulo2>
+      <Labelj> JOGADOR: </Labelj>
+      <Labelg> JOGO: </Labelg>
+
+      <Botaologado> 
+        <BotaoGenerico height = "56px" width ="117px" name= "LIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
+        </Botaologado>
+      
+      
+    </PontoLogado>
+ */
+
+<PontoLogado>
+      <Titulo2>
+      SEU PONTO ESTÁ LIGADO
+      </Titulo2>
+      <Labelj> JOGADOR: </Labelj>
+      <Labelg> JOGO: </Labelg>
+      <Labelb> INÍCIO</Labelb>
+      <Labela> TEMPO </Labela>
+      <Botaologado> 
+        <BotaoGenerico width ="80px" name= "DESLIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
+        </Botaologado>
+      
+      </PontoLogado>
+      
   )
   
+
 }
   
 export default Home;
--- src/Pages/Home/Styles.js
+++ src/Pages/Home/Styles.js
@@ -1,16 +1,87 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Pontoadm = styled.div`
+    background-color: #1F1A1A;
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 70px;
     font-family: 'latoBold';
+    width: 90%;
+    margin-left: 80px;
+    height: 250px;
 `
 
-export const Titulo = styled.div`
+export const Titulo1 = styled.div`
     text-align: center;
     font-size: 25px;
     height: 82px;
+    color: #FFA000;
+`
+export const Botaoadm = styled.button`
+  display: flex;
+    width: auto;
+    flex-direction: column;
+    gap: 25px;
+    margin-top: 50px;
+    border: none;
+    border-radius: 5px;
+`
+export const PontoLogado = styled.div`
+    background-color: #1F1A1A;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 70px;
+    font-family: 'latoBold';
+    width: 90%;
+    margin-left: 80px;
+    height: 250px;
 `
 
+export const Titulo2 = styled.div`
+    text-align: center;
+    font-size: 25px;
+    height: 82px;
+    color: #FFA000;
+    margin-top: 10px;
+`
+
+export const Labelj = styled.p`
+    position: fixed;
+    bottom: 400px;
+    left: 110px;
+    color: #FFA000;
+    
+`
+export const Labelg = styled.p`
+    position: fixed;
+    bottom: 360px;
+    left: 110px;
+    color: #FFA000;
+    
+`
+export const Labela= styled.p`
+    position: fixed;
+    bottom: 400px;
+    right: 500px;
+    color: #FFA000;
+    
+`
+export const Labelb = styled.p`
+    position: fixed;
+    bottom: 360px;
+    right: 500px;
+    color: #FFA000;
+    `
+export const Botaologado = styled.button`
+border: none;
+border-radius: 5px;
+display: flex;
+    width: auto;
+    gap: 25px;
+    margin-top: 50px;
+    position: fixed;
+    bottom: 380px;
+    right: 110px;
+`
