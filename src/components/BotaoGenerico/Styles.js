644b67e985fc09811dc1056dcd4cb723a7392350
lucasliberato9 <lucasliberato2909@gmail.com>
2023-05-21 00:34:57 -0300

    fix: BotaoGenerico Styles

--- src/components/BotaoGenerico/Styles.js
+++ src/components/BotaoGenerico/Styles.js
@@ -1,15 +1,10 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f8f277a (Login e BotaoGenerico atualizados)
     font-family : 'latoBold';
     border: none;
     border-radius: 2px;
 
-<<<<<<< HEAD
     width: ${(props) => props.width};
     height: ${(props) => props.height};
     background-color: ${(props) => props.backgroundColor};
@@ -19,21 +14,4 @@ export const Button = styled.button`
         background-color: ${(props) => props.hoverBackgroundColor};
         cursor: pointer;
     }
-=======
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
-    color: #d9d9d9;
-    background-color: ${(props) => props.color};
->>>>>>> a29e0af (BotaoGenerico com props width e height)
-=======
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
-    background-color: ${(props) => props.backgroundColor};
-    color: ${(props) => props.color};
-
-    &:hover {
-        background-color: ${(props) => props.hoverBackgroundColor};
-        cursor: pointer;
-    }
->>>>>>> f8f277a (Login e BotaoGenerico atualizados)
 `
