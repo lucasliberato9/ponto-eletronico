0d6d98a98eabfffab0919208c3979602d5aec0fb
M-Muniz <matheus.muniz.soares.mms@gmail.com>
2023-05-20 21:41:02 -0300

    Páginas Cadastro e Editar_Perfil prontas

--- src/components/BotaoGenerico/Styles.js
+++ src/components/BotaoGenerico/Styles.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    width: 367px;
+    width: 175px;
     height: 40px;
     color: #d9d9d9;
     background-color: ${(props) => props.color};
+    border: none;
+    border-radius: 2px;
 `
