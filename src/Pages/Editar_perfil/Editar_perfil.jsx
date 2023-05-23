b61fcbb418faa3e4d721c7ec7754259d2c4ea189
lucasliberato9 <lucasliberato2909@gmail.com>
2023-05-23 14:08:01 -0300

    troca de posição dos botões [Cancelar Alterações] e [Confirmar] em editar-perfil

--- src/Pages/Editar_perfil/Editar_perfil.jsx
+++ src/Pages/Editar_perfil/Editar_perfil.jsx
@@ -1,68 +1,75 @@
-import { Container, Titulo, Form, Label, Input, DivInputs, DivRow, DivEmail, DivButton } from "./Styles.js";
+import {
+  Container,
+  Titulo,
+  Form,
+  Label,
+  Input,
+  DivInputs,
+  DivRow,
+  DivEmail,
+  DivButton,
+} from "./Styles.js";
 import { BotaoGenerico } from "../../components";
 
 function Editar_perfil() {
-
   return (
-
     <Container>
-
-      <Titulo>
-        Altere seus dados aqui!
-      </Titulo>
+      <Titulo>Altere seus dados aqui!</Titulo>
 
       <Form>
-
         <DivInputs>
           <DivRow>
             <Label> Nome: </Label>
-            <Input placeholder= "Digite seu nome"></Input>
+            <Input placeholder="Digite seu nome"></Input>
           </DivRow>
 
           <DivRow>
             <Label> Nickname: </Label>
-            <Input placeholder= "Digite seu nickname"></Input>
+            <Input placeholder="Digite seu nickname"></Input>
           </DivRow>
         </DivInputs>
 
         <DivEmail>
           <Label> Email: </Label>
-          <Input placeholder= "exemplo@email.com"></Input>
+          <Input placeholder="exemplo@email.com"></Input>
         </DivEmail>
 
         <DivInputs>
           <DivRow>
             <Label> Jogo: </Label>
-            <Input placeholder= "Digite seu jogo"></Input>
+            <Input placeholder="Digite seu jogo"></Input>
           </DivRow>
 
           <DivRow>
             <Label> Rank/Elo: </Label>
-            <Input placeholder= "Digite seu rank/elo no jogo"></Input>
+            <Input placeholder="Digite seu rank/elo no jogo"></Input>
           </DivRow>
         </DivInputs>
 
         <DivInputs>
           <DivRow>
             <Label> Nova senha: </Label>
-            <Input placeholder= "Escolha uma nova senha"></Input>
+            <Input placeholder="Escolha uma nova senha"></Input>
           </DivRow>
 
           <DivRow>
             <Label> Confirmação da nova senha: </Label>
-            <Input placeholder= "Confirme sua nova senha"></Input>
+            <Input placeholder="Confirme sua nova senha"></Input>
           </DivRow>
         </DivInputs>
-
       </Form>
 
       <DivButton>
-        <BotaoGenerico name= "Cancelar Alterações" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03"/>
-        <BotaoGenerico name= "Confirmar" />
-      </DivButton> 
-
+        <BotaoGenerico name="Confirmar" />
+        <BotaoGenerico
+          name="Cancelar Alterações"
+          backgroundColor="#BC0E03"
+          color="#d9d9d9"
+          hoverBackgroundColor="#990D03"
+        />
+      </DivButton>
     </Container>
-  ) 
+  );
 }
 
 export default Editar_perfil;
