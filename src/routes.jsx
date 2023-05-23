f49437f1b3eb1a7d36c41c1eb0fb957e365522f6
lucasliberato9 <lucasliberato2909@gmail.com>
2023-05-23 13:16:51 -0300

    editar_perfil -> editar-perfil

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import {
   RouterProvider,
   createBrowserRouter,
   Route,
-  createRoutesFromElements
+  createRoutesFromElements,
 } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
@@ -13,14 +13,14 @@ import Editar_perfil from "./Pages/Editar_perfil/Editar_perfil";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path ="/" element = {<Home />} />,
-      <Route path ="cadastro" element = {<Cadastro />} />,
-      <Route path ="login" element = {<Login />} />,
-      <Route path ="editar_perfil" element = {<Editar_perfil />} />
+      <Route path="/" element={<Home />} />,
+      <Route path="cadastro" element={<Cadastro />} />,
+      <Route path="login" element={<Login />} />,
+      <Route path="editar-perfil" element={<Editar_perfil />} />
     </Route>
   )
-)
+);
 
-export default function Routes(){
-  return <RouterProvider router = {router} />
+export default function Routes() {
+  return <RouterProvider router={router} />;
 }
