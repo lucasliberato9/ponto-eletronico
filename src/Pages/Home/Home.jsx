3e3ef3b1129f32800e925594ade35d6c27b35364
Lucas Fernando <lchinuzinhoplay@gmail.com>
2023-05-28 16:49:20 -0300

    atualização home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,93 +1,45 @@
-import {
-  Container,
-  Form,
-  Rodape,
-  Botaoadm,
-  Pontoadm,
-  Titulo1,
-  Botaologado,
-  PontoLogado,
-  Titulo2,
-  Labelj,
-  Labelg,
-  Labelb,
-  Labela,
-  Labelq,
-  Labelw,
-  PontoLigado,
-  Titulo3,
-  Botaoligado,
-  Logados,
-  Time,
-  Dados,
-} from "./Styles";
+import { Container, Form, Rodape, Botaoadm,Pontoadm, Titulo1,
+  Botaologado, PontoLogado, Titulo2, Labelj, Labelg, Labelb, Labela, Labelq,Labelw,
+  PontoLigado,Titulo3, Botaoligado, 
+  Logados, Time, Dados} from "./Styles";
 import { BotaoGenerico } from "../../components";
 
 function Home() {
+
   return (
-    <Container>
-      <Form>
-        <>
-          <Rodape></Rodape>
+<Container>
+    <Form>
+    <Rodape>
 
-          <Pontoadm>
-            <Titulo1>ADMINISTRE SEU PONTO</Titulo1>
-            <Botaoadm>
-              <BotaoGenerico
-                height="70px"
-                width="117px"
-                name="Login/Cadastre-se"
-                backgroundColor="#BC0E03"
-                color="#d9d9d9"
-                hoverBackgroundColor="#990D03"
-              />
-            </Botaoadm>
-          </Pontoadm>
-        </>
-
-        <Logados>
-          <Dados>JOGADOR</Dados>
-          <Dados>JOGO</Dados>
-          <Time>INÍCIO</Time>
-          <Time>TEMPO</Time>
-        </Logados>
-      </Form>
-    </Container>
-
-    // Ligar o ponto
-    /*
+</Rodape>
 <PontoLogado>
-      <Titulo2>
-       LIGUE SEU PONTO
-      </Titulo2>
-      <Labelj> JOGADOR: </Labelj>
-      <Labelg> JOGO: </Labelg>
+  <Titulo2>
+   LIGUE SEU PONTO
+  </Titulo2>
+  <Labelj> JOGADOR: </Labelj>
+  <Labelg> JOGO: </Labelg>
+ 
+<Botaologado>
+<BotaoGenerico height = "100%" width ="100%" name= "LIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
+</Botaologado>
+</PontoLogado>
+<Logados>
+<Dados> JOGADOR</Dados>
+<Dados> JOGO</Dados>
+<Dados> INÍCIO</Dados>
+<Dados> TEMPO</Dados>
+</Logados>
 
-      <Botaologado> 
-        <BotaoGenerico height = "56px" width ="117px" name= "LIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
-        </Botaologado>
-      
-      
-    </PontoLogado>
- */
+    </Form>
+       
+</Container>
 
-    // Desligar o ponto
-    /*
-<PontoLigado>
-      <Titulo3>
-      SEU PONTO ESTÁ LIGADO
-      </Titulo3>
-      <Labelw> JOGADOR: </Labelw>
-      <Labelq> JOGO: </Labelq>
-      <Labelb> INÍCIO</Labelb>
-      <Labela> TEMPO </Labela>
-      <Botaoligado> 
-       <BotaoGenerico height = "56px" width ="117px" name= "Desligar Ponto" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
-        </Botaoligado>
-      
-      </PontoLigado>
-     */
-  );
+
+
+
+  )
+  
+
 }
-
+  
 export default Home;
--- src/Pages/Home/Styles.js
+++ src/Pages/Home/Styles.js
@@ -1,192 +1,252 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    align-items: center;
-    margin-top: 70px;
-    font-family: 'latoBold';
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  margin-top: 70px;
+  font-family: 'latoBold';
 `
 
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    justify-content: center;
-    margin-top: 60px;
-    div {
-        margin-bottom: 15px;
-    }
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  margin-top: 60px;
+
+  div {
+    margin-bottom: 15px;
+  }
 `
 
 export const Rodape = styled.div`
-    background-color: #1F1A1A;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 70px;
-    font-family: 'latoBold';
-    width: 82%;
-    margin-left: 9%;
-    height: 250px;
+  background-color: #1F1A1A;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'latoBold';
+  width: 82%;
+  margin-left: 9%;
+  height: 250px;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-left: 0;
+    height: 200px;
+  }
 `
 
 export const Pontoadm = styled.div`
-    background-color: #1F1A1A;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 70px;
-    font-family: 'latoBold';
-    width: 90%;
-    margin-left: 5%;
-    height: 250px;
+  background-color: #1F1A1A;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 70px;
+  font-family: 'latoBold';
+  width: 90%;
+  margin-left: 5%;
+  height: 250px;
 `
 
 export const Titulo1 = styled.div`
-    text-align: center;
-    font-size: 25px;
-    height: 82px;
-    color: #FFA000;
+  text-align: center;
+  font-size: 25px;
+  height: 82px;
+  color: #FFA000;
 `
 
 export const Botaoadm = styled.button`
-    display: flex;
-    width: auto;
-    flex-direction: column;
-    border: none;
-    border-radius: 5px;
+  display: flex;
+  width: auto;
+  flex-direction: column;
+  border: none;
+  border-radius: 5px;
 `
 
 export const PontoLogado = styled.div`
-    background-color: #1F1A1A;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 70px;
-    font-family: 'latoBold';
-    width: 90%;
-    margin-left: 15px;
-    height: 250px;
+  background-color: #1F1A1A;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 70px;
+  font-family: 'latoBold';
+  width: 90%;
+  margin-left: 5%;
+  height: 250px;
 `
 
 export const Titulo2 = styled.div`
-    text-align: center;
-    font-size: 25px;
-    height: 82px;
-    color: #FFA000;
-    margin-top: 10px;
+  text-align: center;
+  font-size: 25px;
+  height: 82px;
+  color: #FFA000;
+  margin-top: 10px;
 `
 
 export const Labelj = styled.p`
-    color: #FFA000;
+  position: absolute;
+  top: 650px;
+  left: 8%;
+  color: #FFA000;
+  top: 650px;
+
+  @media (max-width: 768px) {
+    top: 580px;
+    left: 30%;
+  }
+  
 `
 
 export const Labelg = styled.p`
-    color: #FFA000;
+  position: absolute;
+  top: 700px;
+  left: 8%;
+  color: #FFA000;
+  
+  position: absolute;
+  
+
+  @media (max-width: 768px) {
+    top: 610px;
+    left: 30%;
+    
+  }
+  
 `
 
 export const Botaologado = styled.button`
-    border: none;
-    border-radius: 5px;
-    display: flex;
-    width: auto;
-    gap: 25px;
-    margin-top: 50px;
+   border: none;
+  border-radius: 5px;
+  display: flex;
+  width: auto;
+  margin-left: 80%;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+    margin-top: 75px;
+    width: 90%;
+    height: 40px;
+  }
 `
 
 export const Botaoligado = styled.button`
-    border: none;
-    border-radius: 5px;
-    display: flex;
-    width: auto;
-    gap: 25px;
-    margin-top: 50px;
+display: flex;
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  width: auto;
+  margin-left: 80%;
 `
 
 export const Titulo3 = styled.div`
-    text-align: center;
-    font-size: 25px;
-    height: 82px;
-    color: #FFA000;
-    margin-top: 10px;
+  text-align: center;
+  font-size: 25px;
+  height: 82px;
+  color: #FFA000;
+  margin-top: 10px;
 `
 
 export const PontoLigado = styled.div`
-    background-color: #1F1A1A;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 70px;
-    font-family: 'latoBold';
-    width: 90%;
-    margin-left: 15px;
-    height: 250px;
+  background-color: #1F1A1A;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 70px;
+  font-family: 'latoBold';
+  width: 90%;
+  margin-left: 5%;
+  height: 250px;
 `
 
 export const Labela = styled.p`
-    color: #FFA000;
+  color: #FFA000;
+  top: 650px;
+  position: absolute;
 `
+
 export const Labelb = styled.p`
-    position: absolute;
-    bottom: 420px;
-    right: 5%;
-    color: #FFA000;
+  top: 700px;
+  color: #FFA000;
+  position: absolute;
 `
 
 export const Labelq = styled.p`
-    position: absolute;
-    bottom: 420px;
-    left: 5%;
-    color: #FFA000;
+  position: absolute;
+  top: 700px;
+  left: 8%;
+  color: #FFA000;
 `
 
 export const Labelw = styled.p`
-    position: absolute;
-    bottom: 450px;
-    left: 5%;
-    color: #FFA000;
+  position: absolute;
+  left: 8%;
+  color: #FFA000;
+  top: 650px;
 `
 
 export const Logados = styled.div`
-    background-color: #1F1A1A;
-    display: flex;
-    margin-top: 70px;
-    font-family: 'latoBold';
-    width: 90%;
-    margin-left: 5%;
-    height: 800px;
-    gap: 20%;
-    justify-content: center;
-    align-items: center;
+  background-color: #1F1A1A;
+  display: flex;
+  margin-top: 20px;
+  font-family: 'latoBold';
+  width: 90%;
+  margin-left: 5%;
+  height: 800px;
+  gap: 20%;
+  justify-content: center;
+  align-items: center;
 
-    @media (max-width: 768px) {
-        flex-direction: column;
-        height: auto;
-    }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    height: auto;
+  }
 `
 
 export const Dados = styled.p`
-    font-size: 20px;
-    color: #FFA000;
-    padding-top: 10px;
-    padding-left: 80px;
+  font-size: 20px;
+  color: #FFA000;
+  padding-top: 10px;
+  padding-left: 80px;
 
-    @media (max-width: 768px) {
-        padding-left: 0;
-        text-align: center;
-        margin-bottom: 10px;
-    }
+  @media (max-width: 768px) {
+    padding-left: 0;
+    text-align: center;
+    margin-bottom: 10px;
+  }
 `
 
 export const Time = styled.p`
-    display: flex;
-    font-size: 20px;
-    color: #FFA000;
-    padding-top: 10px;
+  display: flex;
+  font-size: 20px;
+  color: #FFA000;
+  padding-top: 10px;
 
-    @media (max-width: 768px) {
-        justify-content: center;
-    }
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`
+
+// Responsive adjustments
+export const LabelResponsive = styled.p`
+  color: #FFA000;
+  position: absolute;
+  top: 650px;
+
+  @media (max-width: 768px) {
+    top: 700px;
+  }
+`
+
+export const BotaoligadoResponsive = styled.button`
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  width: auto;
+  margin-left: 80%;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+    margin-top: 10px;
+  }
 `
