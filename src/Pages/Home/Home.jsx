68dc8c349f269924f20c8f69419d4f6b6310fde4
Lucas Fernando <lchinuzinhoplay@gmail.com>
2023-05-28 16:07:11 -0300

    ajustes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,65 +7,36 @@ import { BotaoGenerico } from "../../components";
 function Home() {
 
   return (
-    
- <Container>
-  <Form>
-    <><Rodape>
-      
-    </Rodape>
-     
-      <Pontoadm>
-        <Titulo1>
-          ADMINISTRE SEU PONTO
-        </Titulo1>
-        <Botaoadm>
-          <BotaoGenerico height="70px" width="117px" name="Login/Cadastre-se" backgroundColor="#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
-        </Botaoadm>
-      </Pontoadm></>
+<Container>
+    <Form>
+    <Rodape>
 
-      <Logados>
-         <Dados>JOGADOR</Dados>
-         <Dados>JOGO</Dados>
-         <Time>INÍCIO</Time>
-         <Time>TEMPO</Time>
-      </Logados>
-
-      </Form>
-</Container>
-    
-    // Ligar o ponto
-/*
+</Rodape>
 <PontoLogado>
-      <Titulo2>
-       LIGUE SEU PONTO
-      </Titulo2>
-      <Labelj> JOGADOR: </Labelj>
-      <Labelg> JOGO: </Labelg>
+  <Titulo2>
+   LIGUE SEU PONTO
+  </Titulo2>
+  <Labelj> JOGADOR: </Labelj>
+  <Labelg> JOGO: </Labelg>
+ 
+<Botaologado>
+<BotaoGenerico height = "56px" width ="117px" name= "LIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
+</Botaologado>
+</PontoLogado>
+<Logados>
+<Dados> JOGADOR</Dados>
+<Dados> JOGO</Dados>
+<Dados> INÍCIO</Dados>
+<Dados> TEMPO</Dados>
+</Logados>
 
-      <Botaologado> 
-        <BotaoGenerico height = "56px" width ="117px" name= "LIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
-        </Botaologado>
-      
-      
-    </PontoLogado>
- */
+    </Form>
+       
+</Container>
 
-// Desligar o ponto
-/*
-<PontoLigado>
-      <Titulo3>
-      SEU PONTO ESTÁ LIGADO
-      </Titulo3>
-      <Labelw> JOGADOR: </Labelw>
-      <Labelq> JOGO: </Labelq>
-      <Labelb> INÍCIO</Labelb>
-      <Labela> TEMPO </Labela>
-      <Botaoligado> 
-       <BotaoGenerico height = "56px" width ="117px" name= "Desligar Ponto" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
-        </Botaoligado>
-      
-      </PontoLigado>
-     */
+
+
+
   )
   
 
