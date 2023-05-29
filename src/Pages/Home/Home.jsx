a832e1cbfe05eba5e27e9f4205eed3ebdcd094a1
Lucas Fernando <lchinuzinhoplay@gmail.com>
2023-05-28 21:18:27 -0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,40 +1,40 @@
 import { Container, Form, Rodape, Botaoadm,Pontoadm, Titulo1,
   Botaologado, PontoLogado, Titulo2, Labelj, Labelg, Labelb, Labela, Labelq,Labelw,
   PontoLigado,Titulo3, Botaoligado, 
-  Logados, Time, Dados} from "./Styles";
+  Logados, Time, TimeN, DadosM, DadosJ} from "./Styles";
 import { BotaoGenerico } from "../../components";
 
 function Home() {
 
   return (
-<Container>
+    <Container>
     <Form>
-    <Rodape>
-
-</Rodape>
-<PontoLogado>
-  <Titulo2>
-   LIGUE SEU PONTO
-  </Titulo2>
-  <Labelj> JOGADOR: </Labelj>
-  <Labelg> JOGO: </Labelg>
- 
-<Botaologado>
-<BotaoGenerico height = "100%" width ="100%" name= "LIGAR PONTO" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
-</Botaologado>
-</PontoLogado>
-<Logados>
-<Dados> JOGADOR</Dados>
-<Dados> JOGO</Dados>
-<Dados> INÍCIO</Dados>
-<Dados> TEMPO</Dados>
-</Logados>
-
+      <Rodape>
+  
+      </Rodape>
+      <PontoLigado>
+        <Titulo3>
+        SEU PONTO ESTÁ LIGADO
+        </Titulo3>
+        <Labelw> JOGADOR: </Labelw>
+        <Labelq> JOGO: </Labelq>
+        <Labelb> INÍCIO</Labelb>
+        <Labela> TEMPO </Labela>
+        <Botaoligado> 
+         <BotaoGenerico height = "100%" width ="100%" name= "Desligar Ponto" backgroundColor= "#BC0E03" color="#d9d9d9" hoverBackgroundColor="#990D03" />
+          </Botaoligado>
+        
+        </PontoLigado>
+        <Logados>
+            <DadosM>JOGADOR</DadosM>
+            <DadosJ>JOGO</DadosJ>
+            <Time>INÍCIO</Time>
+            <TimeN>TEMPO</TimeN>
+          </Logados>
+  
+  
     </Form>
-       
-</Container>
-
-
+  </Container>
 
 
   )
