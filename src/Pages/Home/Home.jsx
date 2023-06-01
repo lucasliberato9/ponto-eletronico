786e03c8041df9b0a39304af352c48d50768d3a4
lucasliberato9 <lucasliberato2909@gmail.com>
2023-05-31 21:14:13 -0300

    update home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,53 +1,38 @@
 import {
   Container,
-  Form,
-  Rodape,
-  Botaoadm,
-  Pontoadm,
-  Titulo1,
-  Botaologado,
-  PontoLogado,
-  Titulo2,
-  Labelj,
-  Labelg,
-  Labelb,
-  Labela,
-  Labelq,
-  Labelw,
-  PontoLigado,
-  Titulo3,
-  Botaoligado,
-  Logados
-  
+  TituloPonto,
+  DivImg,
+  MeuPonto,
+  UsuarioPonto,
+  TituloVar,
+  JogadorJogo,
+  NomesVar,
+  ListaPonto,
 } from "./Styles";
 import { BotaoGenerico } from "../../components";
 
 function Home() {
   return (
     <Container>
-      <Form>
-        <Rodape></Rodape>
-        <PontoLigado>
-          <Titulo3>SEU PONTO ESTÁ LIGADO</Titulo3>
-          <Labelw> JOGADOR: </Labelw>
-          <Labelq> JOGO: </Labelq>
-          <Labelb> INÍCIO</Labelb>
-          <Labela> TEMPO </Labela>
-          <Botaoligado>
-            <BotaoGenerico
-              height="100%"
-              width="100%"
-              name="Desligar Ponto"
-              backgroundColor="#BC0E03"
-              color="#d9d9d9"
-              hoverBackgroundColor="#990D03"
-            />
-          </Botaoligado>
-        </PontoLigado>
-        <Logados>
-          
-        </Logados>
-      </Form>
+      <DivImg></DivImg>
+
+      <MeuPonto>
+        <TituloPonto>LIGUE SEU PONTO</TituloPonto>
+
+        <UsuarioPonto>
+          <TituloVar>
+            <JogadorJogo>JOGADOR:</JogadorJogo>
+            <NomesVar>teste</NomesVar>
+          </TituloVar>
+
+          <TituloVar>
+            <JogadorJogo>JOGO:</JogadorJogo>
+            <NomesVar>teste</NomesVar>
+          </TituloVar>
+        </UsuarioPonto>
+      </MeuPonto>
+
+      <ListaPonto></ListaPonto>
     </Container>
   );
 }
--- src/Pages/Home/Styles.js
+++ src/Pages/Home/Styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import imagem_home from "../../assets/imagem_home.png";
 
 export const Container = styled.div`
   display: flex;
@@ -6,243 +7,214 @@ export const Container = styled.div`
   width: 100%;
   align-items: center;
   margin-top: 70px;
-  font-family: 'latoBold';
-`
-
-export const Form = styled.form`
+  font-family: "latoBold";
+`;
+export const TituloPonto = styled.div`
   display: flex;
-  flex-direction: column;
+  color: #ffa000;
+  text-align: center;
+  align-content: center;
+  align-self: center;
+  font-size: 55px;
   width: 100%;
-  justify-content: center;
-  margin-top: 60px;
-
-  div {
-    margin-bottom: 15px;
-  }
-`
-
-export const Rodape = styled.div`
-  background-color: #1F1A1A;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-family: 'latoBold';
-  width: 82%;
-  margin-left: 9%;
-  height: 250px;
-
-  @media (max-width: 768px) {
-    width: 100%;
-    margin-left: 0;
-    height: 200px;
-  }
-`
-
-export const Pontoadm = styled.div`
-  background-color: #1F1A1A;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 70px;
-  font-family: 'latoBold';
-  width: 90%;
-  margin-left: 5%;
-  height: 250px;
-
-  @media (max-width: 768px) {
-  height: 150px;
-  }
-`
-
-export const Titulo1 = styled.div`
-  text-align: center;
-  font-size: 25px;
-  height: 82px;
-  color: #FFA000;
-  margin-top: 10px;
-`
-
-export const Botaoadm = styled.button`
-  display: flex;
-  width: 120px;
-  flex-direction: column;
-  border: none;
-  border-radius: 5px;
-  height: 80px;
-  @media (max-width: 768px) {
-    margin-left: 0;
-    margin-top: 0;
-    width: 90%;
-    height: 40px;
-  }
-`
-
-export const PontoLogado = styled.div`
-  background-color: #1F1A1A;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 70px;
-  font-family: 'latoBold';
-  width: 90%;
-  margin-left: 5%;
-  height: 250px;
-`
-
-export const Titulo2 = styled.div`
-  text-align: center;
-  font-size: 25px;
-  height: 82px;
-  color: #FFA000;
-  margin-top: 10px;
-`
-
-export const Labelj = styled.p`
-  position: absolute;
-  top: 650px;
-  left: 8%;
-  color: #FFA000;
-  top: 650px;
-
-  @media (max-width: 768px) {
-    top: 580px;
-    left: 30%;
-  }
-  
-`
-
-export const Labelg = styled.p`
-  position: absolute;
-  top: 700px;
-  left: 8%;
-  color: #FFA000;
-  
-  position: absolute;
-  
-
-  @media (max-width: 768px) {
-    top: 610px;
-    left: 30%;
-    
-  }
-  
-`
-
-export const Botaologado = styled.button`
-   border: none;
-  border-radius: 5px;
-  display: flex;
-  width: 100px;
-  margin-left: 80%;
-  height: 25%;
-
-  @media (max-width: 768px) {
-    margin-left: 0;
-    margin-top: 75px;
-    width: 90%;
-    height: 40px;
-  }
-`
-
-export const Botaoligado = styled.button`
- border: none;
-  border-radius: 5px;
-  display: flex;
-  width: 100px;
-  margin-left: 80%;
-  height: 25%;
-
-  @media (max-width: 768px) {
-    margin-left: 0;
-    margin-top: 75px;
-    width: 90%;
-    height: 40px;
-  }
-`
-
-export const Titulo3 = styled.div`
-  text-align: center;
-  font-size: 25px;
-  height: 82px;
-  color: #FFA000;
-  margin-top: 10px;
-`
-
-export const PontoLigado = styled.div`
-  background-color: #1F1A1A;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 70px;
-  font-family: 'latoBold';
-  width: 90%;
-  margin-left: 5%;
-  height: 250px;
-`
-
-export const Labela = styled.p`
-  color: #FFA000;
-  top: 650px;
-  position: absolute;
-  @media (max-width: 768px) {
-    top: 630px;
-    left: 60%;
-  }
-`
-
-export const Labelb = styled.p`
-  top: 700px;
-  color: #FFA000;
-  position: absolute;
-  @media (max-width: 768px) {
-    top: 630px;
-    left: 10%;
-  }
-`
-
-export const Labelq = styled.p`
-  position: absolute;
-  top: 700px;
-  left: 8%;
-  color: #FFA000;
-  @media (max-width: 768px) {
-    top: 580px;
-    left: 35%;
-  }
-`
-
-export const Labelw = styled.p`
-  position: absolute;
-  left: 8%;
-  color: #FFA000;
-  top: 650px;
-  @media (max-width: 768px) {
-    top: 560px;
-    left: 35%;
-  }
-`
-
-export const Logados = styled.div`
-  background-color: #1F1A1A;
-  display: flex;
+  height: auto;
   margin-top: 20px;
-  font-family: 'latoBold';
-  width: 90%;
-  margin-left: 5%;
-  height: 800px;
-  gap: 15%;
- 
-  @media (max-width: 768px) {
-    height: auto;
-  }
-`
-export const Jogador = styled.div `
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-margin-top: 100px;
-width: 80%;
-height: 60px;
-background-color: yellow;
-`
+`;
+export const DivImg = styled.div`
+  display: flex;
+  width: 80%;
+  height: 273px;
+  align-self: center;
+`;
+export const MeuPonto = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  align-items: center;
+  background-color: #1f1a1a;
+  height: auto;
+`;
+export const JogadorJogo = styled.div`
+  display: flex;
+  color: #ffa000;
+  text-align: left;
+  font-size: 35px;
+  width: auto;
+  height: auto;
+`;
+export const NomesVar = styled.div`
+  display: flex;
+  color: white;
+  text-align: left;
+  font-size: 35px;
+  width: auto;
+  height: auto;
+`;
+export const TituloVar = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  color: #ffa000;
+  text-align: left;
+  font-size: 35px;
+  width: 100%;
+  height: auto;
+`;
+export const UsuarioPonto = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  gap: 15px;
+  align-items: center;
+  background-color: #1f1a1a;
+  height: auto;
+  margin-top: 20px;
+`;
+export const ListaPonto = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  align-items: center;
+  background-color: #1f1a1a;
+  height: auto;
+`;
+
+// export const Pontoadm = styled.div`
+// background-color: #1f1a1a;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   margin-top: 70px;
+//   font-family: "latoBold";
+//   width: 90%;
+//   margin-left: 5%;
+//   height: 250px;
+
+//   @media (max-width: 768px) {
+//     height: 150px;
+//   }
+// `;
+// export const Titulo1 = styled.div`
+//   text-align: center;
+//   font-size: 25px;
+//   height: 82px;
+//   color: #ffa000;
+//   margin-top: 10px;
+// `;
+// export const Botaoadm = styled.button`
+//   display: flex;
+//   width: 120px;
+//   flex-direction: column;
+//   border: none;
+//   border-radius: 5px;
+//   height: 80px;
+//   @media (max-width: 768px) {
+//     margin-left: 0;
+//     margin-top: 0;
+//     width: 90%;
+//     height: 40px;
+//   }
+// `;
+// export const PontoLogado = styled.div`
+//   background-color: #1f1a1a;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   margin-top: 70px;
+//   font-family: "latoBold";
+//   width: 90%;
+//   margin-left: 5%;
+//   height: 250px;
+// `;
+// export const Labelg = styled.p`
+//   position: absolute;
+//   top: 700px;
+//   left: 8%;
+//   color: #ffa000;
+
+//   position: absolute;
+
+//   @media (max-width: 768px) {
+//     top: 610px;
+//     left: 30%;
+//   }
+// `;
+// export const Botaologado = styled.button`
+//   border: none;
+//   border-radius: 5px;
+//   display: flex;
+//   width: 100px;
+//   margin-left: 80%;
+//   height: 25%;
+
+//   @media (max-width: 768px) {
+//     margin-left: 0;
+//     margin-top: 75px;
+//     width: 90%;
+//     height: 40px;
+//   }
+// `;
+// export const Botaoligado = styled.button`
+//   border: none;
+//   border-radius: 5px;
+//   display: flex;
+//   width: 100px;
+//   margin-left: 80%;
+//   height: 25%;
+
+//   @media (max-width: 768px) {
+//     margin-left: 0;
+//     margin-top: 75px;
+//     width: 90%;
+//     height: 40px;
+//   }
+// `;
+// export const PontoLigado = styled.div`
+//   background-color: #1f1a1a;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   margin-top: 70px;
+//   font-family: "latoBold";
+//   width: 90%;
+//   margin-left: 5%;
+//   height: 250px;
+// `;
+// export const Logados = styled.div`
+//   background-color: #1f1a1a;
+//   display: flex;
+//   margin-top: 20px;
+//   font-family: "latoBold";
+//   width: 90%;
+//   margin-left: 5%;
+// `;
+// export const Labelq = styled.p`
+//   position: absolute;
+//   top: 700px;
+//   left: 8%;
+//   color: #ffa000;
+//   @media (max-width: 768px) {
+//     top: 580px;
+//     left: 35%;
+//   }
+// `;
+// export const Labelw = styled.p`
+//   position: absolute;
+//   left: 8%;
+//   color: #ffa000;
+//   top: 650px;
+//   @media (max-width: 768px) {
+//     top: 560px;
+//     left: 35%;
+//   }
+// `;
+// export const Jogador = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   margin-top: 100px;
+//   width: 80%;
+//   height: 60px;
+//   background-color: yellow;
+// `;
