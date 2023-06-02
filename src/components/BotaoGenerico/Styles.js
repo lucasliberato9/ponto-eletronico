0f659eb49782e621ce2de9212f8c23fab86c4ef8
M-Muniz <matheus.muniz.soares.mms@gmail.com>
2023-06-01 21:44:11 -0300

    Home ATT

--- src/components/BotaoGenerico/Styles.js
+++ src/components/BotaoGenerico/Styles.js
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    font-family : 'latoBold';
-    border: none;
-    border-radius: 2px;
+  font-family: "latoBold";
+  border: none;
+  border-radius: 2px;
 
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
-    background-color: ${(props) => props.backgroundColor};
-    color: ${(props) => props.color};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font-size: ${(props) => props.fontSize};
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.color};
 
-    &:hover {
-        background-color: ${(props) => props.hoverBackgroundColor};
-        cursor: pointer;
-    }
-`
+  &:hover {
+    background-color: ${(props) => props.hoverBackgroundColor};
+    cursor: pointer;
+  }
+
+  @media (max-width: 715px) {
+    width: ${(props) => props.widthResponsive715};
+    height: ${(props) => props.heightResponsive715};
+  }
+`;
